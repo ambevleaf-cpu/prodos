4f6cc88a5adde204a2b847cd8e0ecfda06cc4e1f
aradhay raj <ambevleaf@gmail.com>
2025-11-13 18:21:32 +0000

    make youtube like embedded

--- src/components/apps/YouTube.tsx
+++ src/components/apps/YouTube.tsx
@@ -7,9 +7,11 @@ export default function YouTube() {
     <Card className="w-full h-full border-none shadow-none rounded-none">
       <CardContent className="p-0 h-full">
         <iframe
-          src="https://www.google.com/search?q=youtube&igu=1"
+          src="https://www.youtube.com/tv"
           className="w-full h-full border-0 rounded-b-lg"
-          title="YouTube Search Results"
+          title="YouTube TV"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
         ></iframe>
       </CardContent>
     </Card>
